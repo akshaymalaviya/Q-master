ba86cce5142e2510ed208149248d0716d9f45a78
akshaymalaviya <akshaymalaviya4545@gmail.com>
2022-02-25 22:40:58 +0530

    login validation

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const initialValues = { email: '', password: '' };
+  const [loginData, setLoginData] = useState({ initialValues });
+  //to set errors of data
+  const [loginErrors, setloginErrors] = useState({});
+
+  const InputEvent = (event) => {
+    const { name, value } = event.target;
+    setLoginData({ ...loginData, [name]: value });
+  };
+  const loginForm = (e) => {
+    e.preventDefault();
+    //it will not refresh the page;
+    setloginErrors(validate(loginData));
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Email is required!';
+    }
+    if (!values.password) {
+      errors.password = 'password is required!';
+    }
+    return errors;
+  };
+
   return (
     <>
       <div className="backgroung">
@@ -9,7 +35,7 @@ const Login = () => {
             <div class="content">
               <h2>Q-Master</h2>
               <h3>Sign In</h3>
-              <form>
+              <form onSubmit={loginForm}>
                 <div class="mb-1">
                   <label for="exampleInputEmail1" class="form-label">
                     Email address
@@ -19,11 +45,16 @@ const Login = () => {
                     class="form-control"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
+                    name="email"
+                    value={loginData.email}
+                    onChange={InputEvent}
                   />
                   <div id="emailHelp" class="form-text">
                     We'll never share your email with anyone else.
                   </div>
                 </div>
+                {/* Show the errors */}
+                <p className="formError">{loginErrors.email}</p>
                 <div class="mb-1">
                   <label for="exampleInputPassword1" class="form-label">
                     Password
@@ -31,14 +62,18 @@ const Login = () => {
                   <input
                     type="password"
                     class="form-control"
+                    name="password"
+                    value={loginData.password}
+                    onChange={InputEvent}
                     id="exampleInputPassword1"
                   />
                 </div>
+                {/* Show the errors */}
+                <p className="formError">{loginErrors.password}</p>
                 <button type="submit" class="btn btn-primary">
                   Sign In
                 </button>
               </form>
-              {/* <a href="#">read more</a> */}
             </div>
           </div>
         </div>
--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const initialValues = { email: '', password: '', confirmPassword: '' };
+  const [signupData, setSignupData] = useState({ initialValues });
+  //to set errors of data
+  const [signupErrors, setSignupErrors] = useState({});
+
+  const InputEvent = (event) => {
+    const { name, value } = event.target;
+    setSignupData({ ...signupData, [name]: value });
+  };
+
+  const signupForm = (e) => {
+    //it will not refresh the page;
+    e.preventDefault();
+    //first validate the data and then set errors values
+    setSignupErrors(validate(signupData));
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Email is required!';
+    }
+    if (!values.password) {
+      errors.password = 'password is required!';
+    }
+    if (!values.confirmPassword) {
+      errors.confirmPassword = 'password is required!';
+    }
+    if (values.password !== values.confirmPassword) {
+      errors.comparePassword = 'password must be same!';
+    }
+    return errors;
+  };
+
   return (
     <>
       <div className="backgroung">
@@ -8,8 +42,8 @@ const Signup = () => {
           <div class="card_signup">
             <div class="content">
               <h2>Q-Master</h2>
-              <h3>Sign Un</h3>
-              <form>
+              <h3>Sign Up</h3>
+              <form onSubmit={signupForm}>
                 <div class="mb-1">
                   <label for="exampleInputEmail1" class="form-label">
                     Email address
@@ -19,11 +53,16 @@ const Signup = () => {
                     class="form-control"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
+                    name="email"
+                    value={signupData.email}
+                    onChange={InputEvent}
                   />
                   <div id="emailHelp" class="form-text">
                     We'll never share your email with anyone else.
                   </div>
                 </div>
+                {/* Show the errors */}
+                <p className="formError">{signupErrors.email}</p>
                 <div class="mb-1">
                   <label for="exampleInputPassword1" class="form-label">
                     Password
@@ -32,8 +71,12 @@ const Signup = () => {
                     type="password"
                     class="form-control"
                     id="exampleInputPassword1"
+                    name="password"
+                    value={signupData.password}
+                    onChange={InputEvent}
                   />
                 </div>
+                <p className="formError">{signupErrors.password}</p>
                 <div class="mb-1">
                   <label for="exampleInputPassword1" class="form-label">
                     Confirm Password
@@ -42,8 +85,13 @@ const Signup = () => {
                     type="password"
                     class="form-control"
                     id="exampleInputPassword1"
+                    name="confirmPassword"
+                    value={signupData.confirmPassword}
+                    onChange={InputEvent}
                   />
                 </div>
+                <p className="formError">{signupErrors.comparePassword}</p>
+                <p className="formError">{signupErrors.confirmPassword}</p>
                 <button type="submit" class="btn btn-primary">
                   Sign Up
                 </button>
